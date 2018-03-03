dc0721820bd54787fae7b0bf8d396c179c761198
Lola Simpson <lolalsimp@gmail.com>
2018-03-03 10:36:51 -0600

    added function js

---
+++ function.js
@@ -0,0 +1,32 @@
+
+//function expressions
+
+var a =function(){
+    console.log("a", 3);
+};
+
+//function declarations
+function b(){
+   console.log("b", 2)
+}
+
+
+var num = 3;
+
+function numPrinter(monkeyButt) {
+    //monkeyButt = num =3
+    console.log("my number: ", monkeyButt);
+}
+
+numPrinter(num);
+numPrinter(4);
+numPrinter(6);
+numPrinter ("turtle");
+
+function cat(){
+    return "love them"
+    div.innerHTML = "love them";
+}
+
+var message = cat();
+console.log('message', message);
